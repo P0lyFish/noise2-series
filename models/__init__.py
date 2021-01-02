1ecb25552b99424bbf4c1712eb024c43328cd6af
P0lyFish <tranthephong32@gmail.com>
2021-01-02 18:49:15 +0700

    restructure code to support other methods

---
+++ models/__init__.py
@@ -0,0 +1,16 @@
+import logging
+logger = logging.getLogger('base')
+
+
+def get_trainer(opt):
+    model = opt['trainer']
+    # image restoration
+    if model == 'noise2same':
+        from .noise2self_trainer import Noise2SelfTrainer as M
+    elif model == 'noise2true':
+        from .noise2true_trainer import Noise2TrueTrainer as M
+    else:
+        raise NotImplementedError('Trainer [{:s}] not recognized.'.format(model))
+    m = M(opt)
+    logger.info('Trainer [{:s}] is created.'.format(m.__class__.__name__))
+    return m
