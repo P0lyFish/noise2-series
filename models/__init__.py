bd5dc7ece7019609daa31424c0b306881bebd71b
P0lyFish <tranthephong32@gmail.com>
2021-01-04 09:46:51 +0000

    support noise2void

--- models/__init__.py
+++ models/__init__.py
@@ -11,6 +11,8 @@ def get_trainer(opt):
         from .noise2true_trainer import Noise2TrueTrainer as M
     elif model == 'noise2same':
         from .noise2same_trainer import Noise2SameTrainer as M
+    elif model == 'noise2void':
+        from .noise2void_trainer import Noise2VoidTrainer as M
     else:
         raise NotImplementedError('Trainer [{:s}] not recognized.'.format(model))
     m = M(opt)
