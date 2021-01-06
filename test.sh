63b48e1cb8c311ee7aac7b952666807e59049aef
root <root@sdc2-hanoi.vinai.io-pp.vingroup.local>
2021-01-06 07:56:20 +0000

    fix metrics_evaluation

--- test.sh
+++ test.sh
@@ -1,1 +1,1 @@
-python3 metrics_evaluation.py --model noise2true --LQ_path ../myBSD68/test/noise.npy --HQ_path ../myBSD68/test/clean.npy
+python3 metrics_evaluation.py --num_channels 3 --model noise2true --pretrained_path pretrained_models/ImageNet_noise2true.pth --LQ_path datasets/ImageNet/test/noise.npy --HQ_path datasets/ImageNet/test/clean.npy
