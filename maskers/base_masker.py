a7242fad923d4714e6ad8adc83ba261bd339c8bd
P0lyFish <tranthephong32@gmail.com>
2021-01-08 11:02:28 -0700

    fix bug in masker

--- maskers/base_masker.py
+++ maskers/base_masker.py
@@ -3,9 +3,6 @@ import numpy as np
 
 
 class BaseMasker:
-    def mask(self, X):
-        pass
-
     @staticmethod
     def interpolate_mask(tensor, mask, mask_inv):
         _, num_channels, _, _ = tensor.shape
@@ -19,24 +16,27 @@ class BaseMasker:
         kernel = kernel / kernel.sum()
 
         multi_channel_kernel = torch.zeros((num_channels, num_channels, 3, 3))
-        for i in range(C):
+        for i in range(num_channels):
             multi_channel_kernel[i, i, :, :] = kernel
 
-
-        filtered_tensor = torch.nn.functional.conv2d(tensor, kernel, stride=1, padding=1)
+        filtered_tensor = torch.nn.functional.conv2d(tensor, kernel,
+                                                     stride=1, padding=1)
 
         return filtered_tensor * mask + tensor * mask_inv
 
-    def masked_single_channel(self, X):
+    def mask_single_channel(self, X):
         pass
 
     def mask(self, X):
-        B, C, H, W = X.shape
-        mask = torch.zeros_like(X.shape)
+        batchsz, C, H, W = X.shape
+        mask = torch.zeros((H, W)).to(X.device)
+
+        masked = torch.zeros((batchsz, C, H, W)).to(X.device)
+        mask = torch.zeros((C, H, W)).to(X.device)
 
         for i in range(C):
-            masked_i, mask_i = self.mask_single_channel(X[:, i, :, :])
-            masked[:, i, :, :] = masked_i
-            mask_i[i, :, :] = mask
+            masked_i, mask_i = self.mask_single_channel(X[:, i: i + 1, :, :])
+            masked[:, i, :, :] = masked_i[:, 0, :, :]
+            mask[i, :, :] = mask_i
 
-        return masked_i, mask
+        return masked, mask
